5d0cf4348ac41ec2cbd0722009850f73eb384647
Maicon Novaes <maiconnovaes@gmail.com>
2022-08-17 16:18:53 -0300

    Adicionado Msg no cadastro

--- cadastro.js
+++ cadastro.js
@@ -1,3 +1,15 @@
 function signUp(email){
     return email;
 }
+
+function signUp(telefone){
+    return telefone;
+}
+
+function signUp(endereco){
+    return endereco;
+}
+
+function mostraMensagem(){
+    return "Bem vindo ao Sistema";
+}
